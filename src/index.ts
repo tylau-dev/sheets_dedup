247990f7da7032c43d85000c15445278025d9c50
TY Lau <lautsyyeung@gmail.com>
2021-05-09 18:44:22 +0200

    package json heroku back

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json({
 }))
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000om");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
