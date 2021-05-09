e83f09ace57d34a810275c81864e1db937ddc7ee
TY Lau <lautsyyeung@gmail.com>
2021-05-09 19:15:14 +0200

    change headers

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import driveRouter from "./drive"
 import sheetRouter from "./sheets"
-import cors from "cors"
+import cors from 'cors';
 
 dotenv.config()
 
@@ -22,16 +22,25 @@ app.use(express.json({
 //     res.setHeader('Access-Control-Allow-Methods', 'POST');
 //     res.send();
 // });
-// app.use(cors()) 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
+
+const options: cors.CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'X-Access-Token',
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: "https://sheetdedupggprod.herokuapp.com",
+    preflightContinue: false,
+  };
   
+  //use cors middleware
+  app.use(cors(options));
+  
+
 
 // Default route handler
 app.get("/", async (req, res) => {
