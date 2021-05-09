9ad004cedb37a96141392563f8943793fb4beeaf
TY Lau <lautsyyeung@gmail.com>
2021-05-09 19:02:41 +0200

    install cors

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import driveRouter from "./drive"
 import sheetRouter from "./sheets"
+import cors from "cors"
 
 dotenv.config()
 
@@ -15,21 +16,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json({
     type: ['application/json', 'text/plain']
 }))
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://sheetdedupggprod.herokuapp.com");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });  
 app.options('*', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.send();
 });
+app.use(cors()) 
 
 // Default route handler
 app.get("/", async (req, res) => {
