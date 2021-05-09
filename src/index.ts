d2f45a7e3ad3fc6e62c1cb5cef1f7e7bf5aada59
TY Lau <lautsyyeung@gmail.com>
2021-05-09 18:56:54 +0200

    heroku change CORS

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(express.json({
 }))
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://sheetdedupggprod.herokuapp.com/");
+    res.setHeader("Access-Control-Allow-Origin", "https://sheetdedupggprod.herokuapp.com");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
